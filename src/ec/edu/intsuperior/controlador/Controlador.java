ad91716e22d2f4b357036318771dc5910645ae35
PC John Mena <PC John Mena@192.168.1.3>
2021-06-18 18:41:06 -0500

    Primera versión del Proyecto de Calidad de Software

---
+++ src/ec/edu/intsuperior/controlador/Controlador.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.controlador;
+
+/**
+ *
+ * @author PC John Mena
+ */
+public class Controlador {
+    
+    public static String control(){
+        return "Yo voy a controlar las conexiones existentes";
+    }
+}
---
+++ src/ec/edu/intsuperior/modelo/Conexion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author PC John Mena
+ */
+public class Conexion {
+    
+    public String conectar(){
+        return "Yo me voy a conectar con la base de datos";
+    }
+}
